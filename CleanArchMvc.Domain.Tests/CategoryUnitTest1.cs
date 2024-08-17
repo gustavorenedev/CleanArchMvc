7f95bc1b9efa16096c22d656afb08b73c53670c9
Gustavo René <renegustavo42@gmail.com>
2024-08-17 19:31:01 -0300

    feat: create tests for class product and fix validation for image length

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -23,21 +23,21 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact]
-        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
-        {
-            Action action = () => new Category(1, "");
-            action.Should()
-                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
-                .WithMessage("Invalid name. Name is required");
-        }
-
-        [Fact]
         public void CreateCategory_ShortNameValue_DomainExceptionShortName()
         {
             Action action = () => new Category(1, "Ca");
             action.Should()
                 .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
                 .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
         }
 
         [Fact]
