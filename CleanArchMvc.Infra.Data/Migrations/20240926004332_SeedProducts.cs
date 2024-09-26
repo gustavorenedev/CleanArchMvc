dbd536ef3d6cc33778b4757f676151a2e9489279
Gustavo René <renegustavo42@gmail.com>
2024-09-25 21:49:58 -0300

    feat: migrations application

---
+++ CleanArchMvc.Infra.Data/Migrations/20240926004332_SeedProducts.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Caderno espiral','Caderno espiral 100 folhas',7.45,50,'caderno1.jpg',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Caneta esferográfica','Caneta esferográfica azul',1.99,100,'caneta1.jpg',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Lápis de cor','Caixa de lápis de cor com 12 unidades',12.75,30,'lapis1.jpg',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Calculadora escolar','Calculadora simples',10.99,20,'calculadora1.jpg',2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
