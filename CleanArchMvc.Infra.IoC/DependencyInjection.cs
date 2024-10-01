8fc2485cef25ecfc22ade42413987a8c5d0aa062
Gustavo René <renegustavo42@gmail.com>
2024-09-30 21:50:08 -0300

    feat: include services of Identity

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -2,13 +2,15 @@
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Products.Handlers;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net.NetworkInformation;
 
 namespace CleanArchMvc.Infra.IoC;
 
@@ -20,11 +22,21 @@ public static class DependencyInjection
                                                     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                                                     b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options =>
+                options.AccessDeniedPath = "/Account/Login");
+
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssembly(typeof(ProductCreateCommandHandler).Assembly));
